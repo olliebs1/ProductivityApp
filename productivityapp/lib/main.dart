a6c2b40d2e9985e6d3c631aa86b1b01288bf6ab4
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-03-28 12:52:00 +0000

    wrapped MaterialApp in a new widget named SafeArea to move down navbar and have no over laps

--- productivityapp/lib/main.dart
+++ productivityapp/lib/main.dart
@@ -27,49 +27,51 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      color: Colors.yellow,
-      home: DefaultTabController(
-        length: 4,
-        child: new Scaffold(
-          body: TabBarView(
-            children: [
-              new Container(
-                color: Colors.yellow,
-              ),
-              new Container(
-                color: Colors.orange,
-              ),
-              new Container(
-                color: Colors.lightGreen,
-              ),
-              new Container(
-                color: Colors.red,
-              ),
-            ],
+    return SafeArea(
+      child: MaterialApp(
+        color: Colors.yellow,
+        home: DefaultTabController(
+          length: 4,
+          child: new Scaffold(
+            body: TabBarView(
+              children: [
+                new Container(
+                  color: Colors.yellow,
+                ),
+                new Container(
+                  color: Colors.orange,
+                ),
+                new Container(
+                  color: Colors.lightGreen,
+                ),
+                new Container(
+                  color: Colors.red,
+                ),
+              ],
+            ),
+            appBar: new TabBar(
+              tabs: [
+                Tab(
+                  icon: new Icon(Icons.home),
+                ),
+                Tab(
+                  icon: new Icon(Icons.rss_feed),
+                ),
+                Tab(
+                  icon: new Icon(Icons.perm_identity),
+                ),
+                Tab(
+                  icon: new Icon(Icons.settings),
+                )
+              ],
+              labelColor: Colors.yellow,
+              unselectedLabelColor: Colors.blue,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorPadding: EdgeInsets.all(5.0),
+              indicatorColor: Colors.red,
+            ),
+            backgroundColor: Colors.black,
           ),
-          appBar: new TabBar(
-            tabs: [
-              Tab(
-                icon: new Icon(Icons.home),
-              ),
-              Tab(
-                icon: new Icon(Icons.rss_feed),
-              ),
-              Tab(
-                icon: new Icon(Icons.perm_identity),
-              ),
-              Tab(
-                icon: new Icon(Icons.settings),
-              )
-            ],
-            labelColor: Colors.yellow,
-            unselectedLabelColor: Colors.blue,
-            indicatorSize: TabBarIndicatorSize.label,
-            indicatorPadding: EdgeInsets.all(5.0),
-            indicatorColor: Colors.red,
-          ),
-          backgroundColor: Colors.black,
         ),
       ),
     );
