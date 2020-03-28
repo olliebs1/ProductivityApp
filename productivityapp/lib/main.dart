67a79cc9dcae20b4793361cd2b6c166f3db5015b
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-03-28 14:47:00 +0000

    altered styling for container inside of scaffold stack and added title

--- productivityapp/lib/main.dart
+++ productivityapp/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Productivity App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: MyHomePage(title: 'Productivity App'),
     );
@@ -33,23 +33,40 @@ class _MyHomePageState extends State<MyHomePage> {
         child: DefaultTabController(
           length: 4,
           child: new Scaffold(
-            body: TabBarView(
-              children: [
-                new Container(
-                  color: Colors.yellow,
+            body: Stack(
+              children: <Widget>[
+                TabBarView(
+                  children: [
+                    new Container(
+                      color: Colors.grey,
+                    ),
+                    new Container(
+                      color: Colors.orange,
+                    ),
+                    new Container(
+                      color: Colors.lightGreen,
+                    ),
+                    new Container(
+                      color: Colors.red,
+                    ),
+                  ],
                 ),
-                new Container(
-                  color: Colors.orange,
-                ),
-                new Container(
-                  color: Colors.lightGreen,
-                ),
-                new Container(
-                  color: Colors.red,
-                ),
+                Container(
+                  padding: EdgeInsets.only(left: 50),
+                  height: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(50),
+                          bottomRight: Radius.circular(50))),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[Text("Intray"), Container()]),
+                )
               ],
             ),
             appBar: AppBar(
+              elevation: 0,
               title: new TabBar(
                 tabs: [
                   Tab(
@@ -70,6 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 indicatorSize: TabBarIndicatorSize.label,
                 indicatorPadding: EdgeInsets.all(5.0),
               ),
+              backgroundColor: Colors.white,
             ),
             backgroundColor: Colors.white,
           ),
