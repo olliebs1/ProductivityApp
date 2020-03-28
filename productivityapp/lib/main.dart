6072a45772c46a8b11338e218e473c2a199d81b5
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-03-28 14:25:04 +0000

    altered tabBar into appBar to allow further widgets to be added

--- productivityapp/lib/main.dart
+++ productivityapp/lib/main.dart
@@ -49,28 +49,29 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
-            appBar: new TabBar(
-              tabs: [
-                Tab(
-                  icon: new Icon(Icons.home),
-                ),
-                Tab(
-                  icon: new Icon(Icons.rss_feed),
-                ),
-                Tab(
-                  icon: new Icon(Icons.perm_identity),
-                ),
-                Tab(
-                  icon: new Icon(Icons.settings),
-                )
-              ],
-              labelColor: Colors.yellow,
-              unselectedLabelColor: Colors.blue,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorPadding: EdgeInsets.all(5.0),
-              indicatorColor: Colors.red,
+            appBar: AppBar(
+              title: new TabBar(
+                tabs: [
+                  Tab(
+                    icon: new Icon(Icons.home),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.rss_feed),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.perm_identity),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.settings),
+                  )
+                ],
+                labelColor: Colors.yellow,
+                unselectedLabelColor: Colors.blue,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorPadding: EdgeInsets.all(5.0),
+              ),
             ),
-            backgroundColor: Colors.black,
+            backgroundColor: Colors.white,
           ),
         ),
       ),
