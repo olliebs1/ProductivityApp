7cca46dba4a61db10073b1208b8e352550738b67
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-03-29 22:31:21 +0100

    created intray productivity file containing a stateless widget

---
+++ productivityapp/lib/models/Widgets/intray_productivity_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:productivityapp/models/global.dart';
+
+class IntrayProductivity extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      color: redColor,
+    );
+  }
+}
